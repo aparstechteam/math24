08bc13d15f258190ba92c15bdd30d4f789cbbb91
nasimulhasan <nasimulhasan@gmail.com>
2024-02-09 21:07:44 +0600

    Later

--- assets/axios.540263ef.js
+++
@@ -1,1 +0,0 @@
-import{D as a}from"./index.5477ec20.js";a.defaults.baseURL="https://exam.apars.tech/api";
---
+++ assets/axios.aef0a739.js
@@ -0,0 +1,1 @@
+import{D as a}from"./index.e87744c3.js";a.defaults.baseURL="https://exam.apars.tech/api";
