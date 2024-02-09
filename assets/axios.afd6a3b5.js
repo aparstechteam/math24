911d61e5549f6997e414ac772f762a0cf67d6086
nasimulhasan <nasimulhasan@gmail.com>
2024-02-09 23:57:02 +0600

    leaderboard updated

---
+++ assets/axios.afd6a3b5.js
@@ -0,0 +1,1 @@
+import{D as a}from"./index.144d843b.js";a.defaults.baseURL="http://localhost:3200/api";
--- assets/axios.f963bda4.js
+++
@@ -1,1 +0,0 @@
-import{D as a}from"./index.c8b918c5.js";a.defaults.baseURL="https://exam.apars.tech/api";
