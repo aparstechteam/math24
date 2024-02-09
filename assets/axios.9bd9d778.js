00b53b7bfb06fabb703714d99af2dac39ee1e580
nasimulhasan <nasimulhasan@gmail.com>
2024-02-09 20:47:28 +0600

    updated exm site

---
+++ assets/axios.9bd9d778.js
@@ -0,0 +1,1 @@
+import{D as a}from"./index.a0bd15c6.js";a.defaults.baseURL="https://exam.apars.tech/api";
--- assets/axios.fbe93d3f.js
+++
@@ -1,1 +0,0 @@
-import{D as a}from"./index.247793cd.js";a.defaults.baseURL="https://exam.apars.tech/api";
